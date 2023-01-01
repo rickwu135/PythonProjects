fa43fd726676a13d600fb9451678e7bedbcac92a
Ricky <rickwu135@gmail.com>
2022-12-31 21:44:21 -0500

    bugfix where didn't clear list

--- BlackJack/Blackjack.py
+++ BlackJack/Blackjack.py
@@ -13,10 +13,8 @@
 import random
 import art
 
-
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-user_hand = []
-cpu_hand = []
+
 
 
 def draw():
@@ -55,6 +53,8 @@ def compare(user_score, cpu_score):
 def play():
     """Starts the game"""
     game_over = False
+    user_hand = []
+    cpu_hand = []
     print(art.logo)
     for i in range(2):
         user_hand.append(draw())
